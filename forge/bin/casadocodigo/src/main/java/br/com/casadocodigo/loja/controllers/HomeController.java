c56e44e2782923f8172639d7f37a2b2f1bd86523
Jorge Hech <diordiaum@hotmail.com>
2019-03-25 13:43:31 -0300

    Mapeamento para request do usuário

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,5 +1,17 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/** 
+ * @author Hech
+ * Primeiro CONTROLLER, página inicial
+ */
+@Controller // Spring Annotation @Controller
 public class HomeController {
 
+	@RequestMapping("/") //Mapeamento do request do usuário
+	public void index() {
+		System.out.println("Entrando na home da CDC");
+	}
+	
 }
