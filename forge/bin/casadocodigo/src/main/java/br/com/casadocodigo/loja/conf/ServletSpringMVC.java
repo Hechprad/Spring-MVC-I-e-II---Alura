f7850254852487008524dab1dcc0b41ffc4f62b4
Jorge Hech <diordiaum@hotmail.com>
2019-03-25 13:44:28 -0300

    Classe para mostrar ao servidor que o SPRING vai atender as requisições

---
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -0,0 +1,11 @@
+package br.com.casadocodigo.loja.conf;
+
+/**
+ * @author Hech
+ * Classe para mostrar ao servidor que o SPRING vai atender as requisições
+ */
+public class ServletSpringMVC {
+
+	
+	
+}
