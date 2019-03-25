d8f86755e2acca747775706debd93d3bd496d7a6
Jorge Hech <diordiaum@hotmail.com>
2019-03-25 14:15:16 -0300

    - New Spring Annotations
    configurações iniciais e mostrando qual a classe de controle

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,8 +1,17 @@
 package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+
 /**
  * @author Hech
  *
  */
+
+@EnableWebMvc	//Spring annotation - Configurações iniciais, habilitando o uso do Spring MVC
+@ComponentScan(basePackageClasses= {HomeController.class})	//Mostrando qual a classe de controle
 public class AppWebConfiguration {
 
 }
