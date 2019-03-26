9e362e00eeec99a2b8401847c410e122d81504e2
Jorge Hech <diordiaum@hotmail.com>
2019-03-26 12:05:03 -0300

    entity manager e persistência dos dados produto

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,5 +1,17 @@
 package br.com.casadocodigo.loja.daos;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.casadocodigo.loja.model.Produto;
+
 public class ProdutoDAO {
 
+	@PersistenceContext	//O spring coloca o entity manager aqui com a annotation
+	private EntityManager manager;
+	
+	public void gravar(Produto produto) {	//Gravando produto com JPA
+		manager.persist(produto);
+	}
+	
 }
