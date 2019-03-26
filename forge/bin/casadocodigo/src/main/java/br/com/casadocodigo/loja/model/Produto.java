0ca416a49a672ff0978db771148aa46c77967546
Jorge Hech <diordiaum@hotmail.com>
2019-03-26 10:51:14 -0300

    classe produtos criada no package model para definir as propriedades dos produtos e reescrita do toString

---
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Produto.java
@@ -0,0 +1,34 @@
+package br.com.casadocodigo.loja.model;
+
+public class Produto {
+
+	private String titulo;
+	private String descricao;
+	private int paginas;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [Título = " + titulo + ", descrição = " + descricao + ", páginas = " + paginas + "]";
+	}
+	
+	
+}
