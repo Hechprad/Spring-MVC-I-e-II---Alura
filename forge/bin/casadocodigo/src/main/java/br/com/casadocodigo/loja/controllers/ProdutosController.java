7c48067eb3164417150a4cd119c5ede9d147559c
Jorge Hech <diordiaum@hotmail.com>
2019-03-26 10:50:28 -0300

    método grava adicionado para receber as variáveis do form.jsp no cadastro de produtos

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,12 +3,21 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.model.Produto;
+
 @Controller
 public class ProdutosController {
 
 	@RequestMapping("/produtos/form")
 	public String form() {
 		return "produtos/form";
+	}
+	
+	@RequestMapping("/produtos")
+	public String grava(Produto produto) {
+		System.out.println(produto.toString());
+		
+		return "ok";
 	}
 	
 }
