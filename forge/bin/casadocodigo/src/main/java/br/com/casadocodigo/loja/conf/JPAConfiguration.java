241d4ff557fd35df9ae3c78e11343c42ceef10f1
Jorge Hech <diordiaum@hotmail.com>
2019-03-27 13:28:00 -0300

    Classe de configuração JPA do entityManager para acessar o BD

---
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.casadocodigo.loja.conf;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+/**
+ * @author Hech
+ * Classe que configura o entityManager, e passa as informações para
+ * acessar o BD
+ */
+public class JPAConfiguration {
+
+	@Bean
+	public void entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = 
+				new LocalContainerEntityManagerFactoryBean();
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		
+		factoryBean.setDataSource(dataSource);
+		
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");	//linguagem do banco de dados
+		props.setProperty("hibernate.show_sql", "true");	//Permite que vejamos no console as querys
+		props.setProperty("hibernate.hbm2ddl.auto", "update");	//Sempre que mudarmos uma propriedade o hibernate gerencia e faz um update automático
+		factoryBean.setJpaProperties(props);
+		
+		factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
+	}
+	
+}
