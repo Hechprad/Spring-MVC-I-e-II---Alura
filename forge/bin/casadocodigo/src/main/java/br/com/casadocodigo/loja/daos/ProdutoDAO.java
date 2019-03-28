5e246c7d85cdc94f6ff87146b76ace8f43b8eb97
Jorge Hech <diordiaum@hotmail.com>
2019-03-28 11:25:28 -0300

    método listar com query do BD para produtos

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -17,6 +19,11 @@ public class ProdutoDAO {
 	
 	public void gravar(Produto produto) {	//Gravando produto com JPA
 		manager.persist(produto);
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("select p from Produto p", Produto.class)
+				.getResultList();
 	}
 	
 }
