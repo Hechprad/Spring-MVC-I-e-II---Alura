432c7c1721b7e5421c6123a9f16d21627d4d6630
Jorge Hech <diordiaum@hotmail.com>
2019-04-02 10:26:30 -0300

    carrinho

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.CarrinhoItem;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -8,7 +8,12 @@ public class CarrinhoCompras {
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
 	
 	public void add(CarrinhoItem item) {
-		
+		itens.put(item, getQuantidade(item) + 1);
+	}
+
+	private int getQuantidade(CarrinhoItem item) {
+		if (itens.containsKey(item))
+		return 0;
 	}
 	
 }
