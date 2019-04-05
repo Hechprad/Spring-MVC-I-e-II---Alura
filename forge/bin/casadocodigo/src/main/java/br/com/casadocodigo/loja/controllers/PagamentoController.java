914a29bf79b83202347a64747abb5cc51c2f4e5a
Jorge Hech <diordiaum@hotmail.com>
2019-04-05 19:13:36 -0300

    enviando valor do pagamento para o sistema de recebimento

--- forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ forge/bin/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,12 +17,17 @@ public class PagamentoController {
 	@Autowired
 	private CarrinhoCompras carrinho;
 	
+	private RestTemplate restTemplate;
+	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)	//Aceita apenas método POST
 	public ModelAndView finalizar(RedirectAttributes model) {
-		
+		String uri = "http://book-payment.herokuapp.com/payment";
+		String response = restTemplate.postForObject(uri, carrinho.getTotal(), String.class);//Realiza requisições GET e POST utilizando o protocolo httl para integrar aplicações
 		System.out.println(carrinho.getTotal());
-		model.addFlashAttribute("sucesso", "Pagamento realizado com sucesso!");
+		model.addFlashAttribute("sucesso", response);
 		
 		return new ModelAndView("redirect:/produtos");
 	}
+	
+	
 }
