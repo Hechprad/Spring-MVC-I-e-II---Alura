d491bd2368fba17a74dbd603d63492d075f9e246
Jorge Hech <diordiaum@hotmail.com>
2019-04-20 13:58:37 -0300

    spring test da home e do atributo 'produtos' da home

---
+++ forge/bin/casadocodigo/src/test/java/br/com/casadocodigo/loja/controllers/ProdutosControllerTest.java
@@ -0,0 +1,48 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.casadocodigo.loja.conf.AppWebConfiguration;
+import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
+import br.com.casadocodigo.loja.conf.JPAConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes= {JPAConfiguration.class, 
+		AppWebConfiguration.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
+public class ProdutosControllerTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void deveRetornarParaHomeComOsLivros() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+		.andExpect(MockMvcResultMatchers
+				.model().attributeExists("produtos"))
+		.andExpect(MockMvcResultMatchers
+				.forwardedUrl("/WEB-INF/views/home.jsp"));
+		
+	}
+	
+}
